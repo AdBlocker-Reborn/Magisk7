5d432fda4c2dd122383aa4b7f1a2f4cfacf38f2b
Wang Han <1701210638@pku.edu.cn>
2024-08-10 23:05:51 +0800

    Update rootdir.rs

--- native/src/init/rootdir.rs
+++ native/src/init/rootdir.rs
@@ -14,18 +14,13 @@ pub fn inject_magisk_rc(fd: RawFd, tmp_dir: &Utf8CStr) {
         file,
         r#"
 on post-fs-data
-    exec {0} 0 0 -- {1}/magisk --post-fs-data
-
-on property:vold.decrypt=trigger_restart_framework
-    exec {0} 0 0 -- {1}/magisk --service
-    exec {0} 0 0 -- {1}/magisk --stop
-
-on nonencrypted
-    exec {0} 0 0 -- {1}/magisk --service
-    exec {0} 0 0 -- {1}/magisk --stop
-"#,
-    "u:r:magisk:s0", tmp_dir
-    )
+    start logd
+    umount /debug_ramdisk
+    umount /system/etc/init/hw/init.zygote64_32.rc
+    umount /system/etc/init/hw/init.zygote32.rc
+    umount /system/etc/init/hw/init.rc
+    mount none /system/bin/app_process64 /system/bin/app_process64 bind rec   
+"#)
     .ok();
 
     mem::forget(file)
